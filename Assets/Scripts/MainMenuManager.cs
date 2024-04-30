1b0b772f37db594751bcba9931a70d24afdc4e5f
Noel Christevent Mandak <noelmandak03@gmail.com>
2024-04-30 21:50:01 +0700

    Add UI

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public TextMeshProUGUI usernameText;
+    public Text usernameText;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/NavigationManager.cs
+++ Assets/Scripts/NavigationManager.cs
@@ -11,4 +11,52 @@ public class NavigationManager : MonoBehaviour
         AudioManager.Instance.PlaySFX("buttonpress1");
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void OnLogoutButtonClick()
+    {
+        AudioManager.Instance.PlaySFX("buttonpress1");
+        // Menghapus username yang tersimpan
+        PlayerPrefs.DeleteKey("Username");
+
+        // Pindah ke scene LoginSignup
+        SceneManager.LoadScene("LoginSignup");
+    }
+    // Fungsi untuk tombol Play
+    public void OnPlayButtonClick()
+    {
+        AudioManager.Instance.PlaySFX("buttonpress1");
+        // Pindah ke scene Game
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Fungsi untuk tombol Player History
+    public void OnScoreBoardButtonClick()
+    {
+        AudioManager.Instance.PlaySFX("buttonpress1");
+        // Pindah ke scene Player History
+        SceneManager.LoadScene("ScoreBoard");
+    }
+    // Fungsi untuk tombol Player History
+    public void OnPlayerHistoryButtonClick()
+    {
+        AudioManager.Instance.PlaySFX("buttonpress1");
+        // Pindah ke scene Player History
+        SceneManager.LoadScene("PlayerHistory");
+    }
+
+    // Fungsi untuk tombol Settings
+    public void OnSettingsButtonClick()
+    {
+        AudioManager.Instance.PlaySFX("buttonpress1");
+        // Pindah ke scene Settings
+        SceneManager.LoadScene("Settings");
+    }
+
+    // Fungsi untuk tombol Exit
+    public void OnExitButtonClick()
+    {
+        // Keluar dari aplikasi (hanya berfungsi di build)
+        AudioManager.Instance.PlaySFX("buttonpress1");
+        Application.Quit();
+    }
 }
